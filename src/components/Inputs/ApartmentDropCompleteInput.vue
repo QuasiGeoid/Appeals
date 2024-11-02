9cb4dd648c1b781ee72014ab065536a045a9d240
QuasiGeoid <[200~kotofeykotofeich526@gmail.comm>
2024-11-02 14:08:00 +0300

    create component, remove mixins

---
+++ src/components/Inputs/ApartmentDropCompleteInput.vue
@@ -0,0 +1,76 @@
+<template>
+  <DropCompleteInput
+    v-model="apartmentId"
+    :options="apartmentSelectList"
+    placeholder="Квартира"
+    :loading="loadingApartments"
+    @search="handleSearchApartmentUpdate"
+    @input="selectApartment"
+  />
+</template>
+
+<script>
+import { mapGetters } from "vuex";
+import debounce from "lodash.debounce";
+import { DEBOUNCE_DELAY } from "@/utils/constants";
+import { DropCompleteInput } from "@/components/Inputs";
+
+export default {
+  name: "ApartmentDropCompleteInput",
+  components: { DropCompleteInput },
+  props: {
+    value: {
+      type: [String, Number],
+      default: "",
+    },
+    premiseId: {
+      type: String,
+      default: "",
+    },
+  },
+  data() {
+    return {
+      searchApartment: "",
+      apartmentId: this.value,
+      localPremiseId: this.premiseId,
+      loadingApartments: false,
+    };
+  },
+  computed: {
+    ...mapGetters(["apartmentSelectList"]),
+  },
+  watch: {
+    value(newVal) {
+      this.apartmentId = newVal;
+    },
+    premiseId(newVal) {
+      this.localPremiseId = newVal;
+      this.fetchApartments();
+    },
+  },
+  methods: {
+    fetchApartments: debounce(async function () {
+      console.log("here");
+      if (!this.localPremiseId) return;
+
+      const params = {
+        premise_id: this.localPremiseId,
+        search: this.searchApartment,
+      };
+      this.loadingApartments = true;
+      await this.$store.dispatch("fetchApartments", params);
+      this.loadingApartments = false;
+    }, DEBOUNCE_DELAY),
+    handleSearchApartmentUpdate(searchTerm) {
+      this.searchApartment = searchTerm;
+      this.fetchApartments();
+    },
+    selectApartment(apartmentId) {
+      this.$emit("input", apartmentId);
+    },
+  },
+  mounted() {
+    this.fetchApartments(this.localPremiseId);
+  },
+};
+</script>
--- src/components/Inputs/index.js
+++ src/components/Inputs/index.js
@@ -3,3 +3,5 @@ export { default as DropCompleteInput } from "./DropCompleteInput.vue";
 export { default as FilterInput } from "./FilterInput.vue";
 export { default as LoginInput } from "./LoginInput.vue";
 export { default as PasswordInput } from "./PasswordInput.vue";
+export { default as PremiseDropCompleteInput } from "./PremiseDropCompleteInput.vue";
+export { default as ApartmentDropCompleteInput } from "./ApartmentDropCompleteInput.vue";
