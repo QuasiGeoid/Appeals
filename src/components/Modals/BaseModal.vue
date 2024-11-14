434d03a22463e7cea6fc3a648946bc2c2eb61c65
QuasiGeoid <[200~kotofeykotofeich526@gmail.comm>
2024-11-15 00:21:01 +0300

    create BaseModal

---
+++ src/components/Modals/BaseModal.vue
@@ -0,0 +1,50 @@
+<template>
+  <div v-if="show" class="base-modal">
+    <BaseOverlay @close="handleClose" />
+    <div class="base-modal__content">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+import { BaseOverlay } from "@/components/common";
+
+export default {
+  name: "BaseModal",
+  components: { BaseOverlay },
+  props: {
+    show: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  methods: {
+    handleClose() {
+      this.$emit("close");
+    },
+  },
+};
+</script>
+
+<style lang="sass" scoped>
+.base-modal
+  transition: opacity 0.3s ease
+  position: fixed
+  top: 0
+  left: 0
+  width: 100vw
+  height: 100vh
+
+  &__content
+    position: fixed
+    top: 50%
+    left: 50%
+    transform: translate(-50%, -50%)
+    justify-content: center
+    align-items: center
+    background-color: $color-background-primary
+    padding: 1.5rem 2rem
+    border-radius: 0.5rem
+    z-index: $z-index-modal
+</style>
