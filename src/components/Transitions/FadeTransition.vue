f44ba2aacb4ddd0a5634703a2df789eec0f279a0
QuasiGeoid <[200~kotofeykotofeich526@gmail.comm>
2024-11-14 23:13:53 +0300

    create FadeTransition component

---
+++ src/components/Transitions/FadeTransition.vue
@@ -0,0 +1,22 @@
+<template>
+  <transition name="fade">
+    <slot></slot>
+  </transition>
+</template>
+
+<script>
+export default {
+  name: "FadeTransition",
+};
+</script>
+
+<style lang="sass" scoped>
+.fade
+  &-enter-active,
+  &-leave-active
+    transition: opacity 0.5s ease
+
+  &-enter,
+  &-leave-to
+    opacity: 0
+</style>
